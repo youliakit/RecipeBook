ab3d7f22b5637f1bf8db057465e48559d35e971e
Youlia🦋 <182519876+youliakit@users.noreply.github.com>
2026-02-28 15:18:06 +0400

    Add new file to edit one direction at a time

---
+++ RecipeBook/Views/ModifyRecipes/ModifyDirectionView.swift
@@ -0,0 +1,45 @@
+//
+//  ModifyDirectionView.swift
+//  RecipeBook
+//
+//  Created by 𝒴𝑜𝓊𝓁𝒾𝒶 𝒯𝒾𝑔𝓇𝒶𝓃𝓎𝒶𝓃 on 27.02.26.
+//  Changes one direction at a time
+
+import SwiftUI
+
+struct ModifyDirectionView: View {
+	@Binding var direction: Direction // Using @Binding to edit Direction inside of Recipe structure, without it we would edit the copy
+	let createAction: (Direction) -> Void // allows parent(Recipe model) to decide what 'Save' means
+
+	private let listBackgroundColor = AppColor.background
+	private let listTextColor = AppColor.foreground
+
+	@Environment(\.presentationMode) private var mode // gives access to the sheet's presentation state
+
+	var body: some View {
+		Form {
+			TextField("Direction Description", text: $direction.description) // direction is Binding<Direction>, but SwiftUI supports dynamic member lookup so we do $direction.description and it becomes Binding<String>
+				.listRowBackground(listBackgroundColor)
+			Toggle("Optional", isOn: $direction.isOptional)
+				.listRowBackground(listBackgroundColor)
+			HStack {
+				Spacer()
+				Button("Save") {
+					createAction(direction) // When "Save" is pressed we pass the edited direction back to parent and parent decides what to do
+					mode.wrappedValue.dismiss() // telling SwiftUI to close this sheet
+				}
+				Spacer()
+			}.listRowBackground(listBackgroundColor)
+		}
+		.foregroundColor(listTextColor)
+	}
+}
+
+struct ModifyDirectionView_Previews: PreviewProvider {
+	@State static var emptyDirection = Direction(description: "", isOptional: false)
+
+	static var previews: some View {
+		NavigationView { ModifyDirectionView(direction: $emptyDirection) { _ in return }
+		}
+	}
+}
